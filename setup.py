9352d3cdaeced21c4888d008958eb21976130ce6
Aminul Islam <aminulislam.stat@gmail.com>
2023-03-12 21:14:12 +0600

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN__E_DOT = '-e .'
+
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN__E_DOT in requirements:
+            requirements.remove(HYPHEN__E_DOT)
+
+    return requirements
+
+setup(
+name='mlproject',
+vrsion='0.0.1',
+author='Aminul Islam',
+author_email='aminulislam.stat@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt'),
+)
